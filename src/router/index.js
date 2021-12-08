a0fd44af765c9cf4b06b7b4b73e4511e14705f2d
Kaviilee <jiaweilee95@126.com>
2021-12-08 19:45:09 +0800

    fix: 路由重复点击

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+// 解决重复点击路由报错的BUG
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
+
 const Index = () => import("../views/Index.vue");
 const Namespace = () => import("../views/Namespace.vue");
 const Joke = () => import("../views/Joke.vue");
