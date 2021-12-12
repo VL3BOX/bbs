7880a689d2aa189bd01d3e592de7879fb14bce8f
iRuxu <rx6@qq.com>
2021-12-12 16:04:15 +0800

    fix:joke sidebar

--- src/App.vue
+++ src/App.vue
@@ -31,11 +31,13 @@ import Info from "@/components/Info.vue";
 import Nav from "@/components/nav/Nav.vue";
 import publishGate from "@/components/publish_gate.vue";
 import { getAppIcon, getAppType } from "@jx3box/jx3box-common/js/utils";
+import {single_types} from '../setting.json'
 export default {
     name: "App",
     props: [],
     data: function () {
-        return {};
+        return {
+        };
     },
     computed: {},
     methods: {
@@ -44,7 +46,7 @@ export default {
             let arr = location.href.replace('#','/').split("/");
             let id = 0;
             for (let i = arr.length - 1; i > -1; i--) {
-                if(arr[i]){
+                if(arr[i] && !isNaN(arr[i])){
                     id = arr[i]
                     break;
                 }
@@ -60,7 +62,7 @@ export default {
     created: function () {
         let type = getAppType();
         let id = this.getAppId()
-        if (type && type != "bbs") {
+        if (type && single_types.includes(type)) {
             this.$router.push({
                 name: type,
                 params: {
--- src/components/nav/Nav.vue
+++ src/components/nav/Nav.vue
@@ -1,26 +1,38 @@
 <template>
     <div>
-        <list_nav v-if="mode == 'list'" class="m-nav"/>
-        <single_nav v-else />
+        <list_nav v-if="mode == 'list'" class="m-nav" />
+        <single_nav v-if="mode == 'single'" />
     </div>
 </template>
 
 <script>
 import list_nav from "@/components/nav/list_nav.vue";
 import single_nav from "@/components/nav/single_nav.vue";
+import { single_types } from "../../../setting.json";
 export default {
     name: "Nav",
     props: [],
     components: {
         list_nav,
-        single_nav
+        single_nav,
     },
     data: function () {
         return {};
     },
     computed: {
+        isLikeSinglePage: function () {
+            return !!this.$route.params.id;
+        },
+        type: function () {
+            return this.$route.name;
+        },
+        isLikeSingleType: function () {
+            return single_types.includes(this.type);
+        },
         mode: function () {
-            return this.$route.params.id ? "single" : "list";
+            return this.isLikeSinglePage && this.isLikeSingleType
+                ? "single"
+                : "list";
         },
     },
     watch: {},
