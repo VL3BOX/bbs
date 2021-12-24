e45a558c37aa1f06055853aa91e673f54da8d3df
iRuxu <rx6@qq.com>
2021-12-25 05:59:39 +0800

    fix:exam single header

--- src/components/exam/question_single.vue
+++ src/components/exam/question_single.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="v-question-single">
-    <SingleTitle :item="data" type="question" />
-    <SingleCard :item="data" :answer="answer" :userAnswers="userAnswers" :isSubmitted="isSubmitted" @changeVal="finalAnswer" />
-    <div class="u-submit" @click="submit">
-      <el-button class="u-btn">提交</el-button>
+    <div class="v-question-single">
+        <SingleTitle :item="data" type="question" />
+        <SingleCard :item="data" :answer="answer" :userAnswers="userAnswers" :isSubmitted="isSubmitted" @changeVal="finalAnswer" />
+        <div class="u-submit" @click="submit">
+            <el-button class="u-btn">提交</el-button>
+        </div>
+        <div class="m-mark" v-if="!isLogin" @click="prompt"></div>
     </div>
-    <div class="m-mark" v-if="!isLogin" @click="prompt"></div>
-  </div>
 </template>
 <script>
 import SingleCard from "@/components/exam/single_card.vue";
@@ -14,82 +14,82 @@ import SingleTitle from "@/components/exam/single_title.vue";
 import { getQuestion, submitQuestionAnswer } from "@/service/exam.js";
 import User from "@jx3box/jx3box-common/js/user";
 export default {
-  name: "QuestionSingle",
-  props: [],
-  components: { SingleCard, SingleTitle },
-  data: function () {
-    return {
-      isLogin: "",
-      data: {},
-      answer: "",
-      userAnswers: {},
-      submitAnswers: {},
-      isSubmitted: false
-    };
-  },
-  computed: {},
-  watch: {},
-  methods: {
-    loadData: function () {
-      let id = this.$route.params.id;
-      getQuestion(id).then((res) => {
-        let data = res.data;
-        data.tags = JSON.parse(data.tags);
-        data.options = JSON.parse(data.options);
-        this.data = data;
-      });
+    name: "QuestionSingle",
+    props: [],
+    components: { SingleCard, SingleTitle },
+    data: function() {
+        return {
+            isLogin: "",
+            data: {},
+            answer: "",
+            userAnswers: {},
+            submitAnswers: {},
+            isSubmitted: false,
+        };
     },
-    finalAnswer: function (val) {
-      let key, value;
+    computed: {},
+    watch: {},
+    methods: {
+        loadData: function() {
+            let id = this.$route.params.id;
+            getQuestion(id).then((res) => {
+                let data = res.data;
+                data.tags = JSON.parse(data.tags);
+                data.options = JSON.parse(data.options);
+                this.data = data;
+            });
+        },
+        finalAnswer: function(val) {
+            let key, value;
 
-      for (var i in val) {
-        key = i;
-        if (val[i].length) {
-          value = val[i].sort();
+            for (var i in val) {
+                key = i;
+                if (val[i].length) {
+                    value = val[i].sort();
+                }
+                value = val[i];
+            }
+
+            let arr = [];
+            for (let i = 0; i < this.data.options.length; i++) {
+                for (let j = 0; j < value.length; j++) {
+                    if (value[j] == i) {
+                        arr.push(this.data.options[i]);
+                    }
+                }
+            }
+
+            this.submitAnswers = arr;
+            this.$set(this.userAnswers, key, value);
+        },
+        prompt() {
+            this.$message.error("请先登录");
+        },
+        submit() {
+            if (JSON.stringify(this.userAnswers) == "{}") {
+                this.$alert("你没有选择答案哦~", "提交失败", {
+                    type: "error",
+                });
+            } else {
+                submitQuestionAnswer(this.$route.params.id, this.submitAnswers).then((res) => {
+                    if (res.data) {
+                        this.answer = res.data;
+                        this.isSubmitted = true;
+                    }
+                });
+            }
+        },
+    },
+    filters: {},
+    created: function() {
+        if (!User.isLogin()) {
+            this.prompt();
+            this.isLogin = false;
+        } else {
+            this.isLogin = true;
         }
-        value = val[i];
-      }
-
-      let arr = []
-      for (let i = 0; i < this.data.options.length; i++) {
-        for (let j = 0; j < value.length; j++) {
-          if (value[j] == i) {
-            arr.push(this.data.options[i])
-          }
-        }
-      }
-
-      this.submitAnswers = arr
-      this.$set(this.userAnswers, key, value);
+        this.loadData();
     },
-    prompt () {
-      this.$message.error("请先登录");
-    },
-    submit () {
-      if (JSON.stringify(this.userAnswers) == "{}") {
-        this.$alert("你没有选择答案哦~", "提交失败", {
-          type: "error",
-        });
-      } else {
-        submitQuestionAnswer(this.$route.params.id, this.submitAnswers).then((res) => {
-          if (res.data) {
-            this.answer = res.data
-            this.isSubmitted = true
-          }
-        })
-      }
-    },
-  },
-  filters: {},
-  created: function () {
-    if (!User.isLogin()) {
-      this.prompt();
-      this.isLogin = false;
-    } else {
-      this.isLogin = true;
-    }
-    this.loadData();
-  },
 };
 </script>
 
