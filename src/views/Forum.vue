c3796c3fe266ac0b44f97b1773f4eaefb6b4b3f3
iRuxu <rx6@qq.com>
2022-01-22 13:57:44 +0800

    fix:loadmore

--- src/views/Forum.vue
+++ src/views/Forum.vue
@@ -98,15 +98,19 @@ export default {
         // 请求关联参数
         query: function() {
             return {
-                page: this.page,
-                per: this.per,
-
                 subtype: this.subtype,
                 order: this.order,
                 mark: this.mark,
                 client: this.client,
                 search: this.search,
             };
+        },
+        // 分页相关参数
+        pg_queries : function (){
+            return {
+                page: this.page,
+                per: this.per,
+            }
         },
         // 重置页码参数
         reset_queries: function() {
@@ -116,9 +120,14 @@ export default {
     methods: {
         // 构建最终请求参数
         buildQuery: function(appendMode) {
+            if (appendMode) {
+                this.page += 1
+            }
             let _query = {
-                page: this.page,
+                per : this.per,
+                page : this.page,
             };
+
             for (let key in this.query) {
                 if (this.query[key] !== undefined && this.query[key] !== "" && this.query[key] !== null) {
                     if (key == "search") {
@@ -133,9 +142,6 @@ export default {
             // 当指定子类别时启用置顶
             if(_query.subtype){
                 _query.sticky = 1
-            }
-            if (appendMode) {
-                _query.page += 1;
             }
             return _query;
         },
@@ -178,6 +184,7 @@ export default {
         },
         // 翻页加载
         changePage: function(i) {
+            this.loadData();
             this.replaceRoute({ page: i });
         },
         // 追加加载
