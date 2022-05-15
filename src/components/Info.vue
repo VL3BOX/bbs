08825c057131064f1b673a962f6e2c8f70af8dd9
iRuxu <rx6@qq.com>
2022-05-15 11:31:04 +0800

    fix:top router

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,24 +1,30 @@
 <template>
     <div class="m-info">
-        <div class="m-info-router">
+        <div class="m-info-router" v-if="isRouter">
             <!-- <router-link to="/"><i class="el-icon-receiving"></i></router-link> -->
             <router-link to="/forum"><i class="el-icon-collection"></i></router-link>
             <router-link to="/joke"><i class="el-icon-cold-drink"></i></router-link>
             <router-link to="/emotion"><i class="el-icon-sugar"></i></router-link>
             <router-link to="/namespace"><i class="el-icon-postcard"></i></router-link>
             <router-link to="/collection"><i class="el-icon-paperclip"></i></router-link>
-            <router-link to="/exam"><i class="el-icon-document"></i></router-link>
+            <!-- <a href="/exam"><i class="el-icon-document"></i></a> -->
         </div>
     </div>
 </template>
 
 <script>
+import {getAppType} from '@jx3box/jx3box-common/js/utils'
 export default {
     name: "Info",
     props: [],
     data: function() {
         return {};
     },
+    computed : {
+        isRouter : function (){
+            return getAppType() != 'exam'
+        }
+    }
 };
 </script>
 
