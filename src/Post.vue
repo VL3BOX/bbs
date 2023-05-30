b819df1306af9ef7ca79d0d30ab6ffab390fb2e4
Kaviilee <jiaweilee95@126.com>
2023-05-30 22:07:13 +0800

    feat: update pkg

--- src/Post.vue
+++ src/Post.vue
@@ -3,6 +3,9 @@
         <Header></Header>
         <Breadcrumb name="茶馆交流" slug="bbs" :publishEnable="false" :adminEnable="true" :feedbackEnable="true" :crumbEnable="true">
             <publish-gate slot="op-append" />
+            <template #op-prepend>
+                <AdminDirectMessage :user-id="user_id" :sourceId="post.ID" :sourceType="post.post_type"></AdminDirectMessage>
+            </template>
         </Breadcrumb>
         <LeftSidebar :uid="user_id">
             <Nav :id="id" />
@@ -23,6 +26,7 @@ import Side from "@/components/bbs/single_side.vue";
 import single from "@/components/bbs/single.vue";
 import publishGate from "@/components/publish_gate.vue";
 import { getAppIcon,getAppID } from "@jx3box/jx3box-common/js/utils";
+import AdminDirectMessage from "@jx3box/jx3box-common-ui/src/bread/AdminDirectMessage.vue"
 export default {
     name: "App",
     props: [],
@@ -35,6 +39,9 @@ export default {
         user_id: function () {
             return this.$store.state.user_id;
         },
+        post: function () {
+            return this.$store.state.post;
+        },
     },
     methods: { getAppIcon },
     components: {
@@ -42,6 +49,7 @@ export default {
         Side,
         single,
         "publish-gate": publishGate,
+        AdminDirectMessage
     },
 };
 </script>
