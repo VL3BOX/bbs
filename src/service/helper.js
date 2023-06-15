e645ee268b338345d3bc03c4d0d514f57c3005f4
Kaviilee <jiaweilee95@126.com>
2023-06-16 00:13:46 +0800

    chore: rm unuse file

--- src/service/helper.js
+++ src/service/helper.js
@@ -1,29 +1,4 @@
 import { $helper } from "@jx3box/jx3box-common/js/https";
-import qs from "qs";
-
-function getCollection($collection_id, params = {}) {
-    return $helper()({
-        method: "GET",
-        url: `/api/post/collection/${$collection_id}`,
-        params: params,
-    });
-}
-
-function getCollections(params) {
-    return $helper()({
-        method: "GET",
-        url: `/api/post/collections`,
-        params: params,
-    });
-}
-
-function removeCollection(collection_id) {
-    return $helper()({
-        method: "PUT",
-        url: `/api/post/collection/remove`,
-        data: qs.stringify({ id: collection_id }),
-    });
-}
 
 function getMenus(key){
     return $helper()({
@@ -32,4 +7,4 @@ function getMenus(key){
     });
 }
 
-export { getCollection, getCollections, removeCollection, getMenus };
+export { getMenus };
